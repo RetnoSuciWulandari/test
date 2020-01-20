764e32a94c3690eac06591dd9472d0de6e7c6baa
Retno <retnoscwulandari@gmail.com>
2020-01-20 16:51:10 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,133 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "core-js/modules/es.array.find";
+import "core-js/modules/es.array.find-index";
+import "core-js/modules/es.promise";
+import "core-js/modules/es.string.includes";
+import "core-js/modules/es.object.assign";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+import MultiSelectBox from "react-multiselect-box";
+import "react-multiselect-box/build/css/index.css";
+//import "./styles.css";
+
+const getOptions = qty => {
+  let options = [];
+  for (let i = 0; i < qty; i++) {
+    options.push({
+      desc: "Item " + i,
+      value: i
+    });
+  }
+  return options;
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedOne: [],
+      selectedTwo: []
+    };
+  }
+
+  render() {
+    const { selectedOne, selectedTwo } = this.state;
+    return (
+      <div className="container">
+        <h5>Example 1</h5>
+
+        <MultiSelectBox
+          options={[
+            {
+              desc:
+                "Item 1hfkdshfkshfjkdfhsjkfhjkdhdjksfdjkhgjkghdsjkghsdjkghdskjgh",
+              value: "1"
+            },
+            {
+              desc:
+                "Item 2hfkdshfkshfjkdfhsjkfhjkdhdjksfdjkhgjkghdsjkghsdjkghdskjghhfkdshfkshfjkdfhsjkfhjkdhdjksfdjkhgjkghdsjkghsdjkghdskjgh",
+              value: "2"
+            },
+            {
+              desc:
+                "Item 3hfkdshfkshfjkdfhsjkfhjkdhdjksfdjkhgjkghdsjkghsdjkghdskjghhfkdshfkshfjkdfhsjkfhjkdhdjksfdjkhgjkghdsjkghsdjkghdskjgh",
+              value: "3"
+            }
+          ]}
+          labelKey="desc"
+          valueKey="value"
+          onAdd={selectedItem => {
+            this.setState({
+              selectedOne: [...this.state.selectedOne, selectedItem.value]
+            });
+          }}
+          onRemove={(removedItem, index) => {
+            this.setState({
+              selectedOne: [
+                ...this.state.selectedOne.filter(
+                  item => item !== removedItem.value
+                )
+              ]
+            });
+          }}
+          onSelectAll={selectedItems => {
+            this.setState({
+              selectedOne: [
+                ...this.state.selectedOne,
+                ...selectedItems.map(item => item.value)
+              ]
+            });
+          }}
+          onRemoveAll={() =>
+            this.setState({
+              selectedOne: []
+            })
+          }
+          valueArray={selectedOne}
+        />
+        <h5>Example 2</h5>
+        <MultiSelectBox
+          options={getOptions(2000)}
+          labelKey="desc"
+          valueKey="value"
+          valueArray={selectedTwo}
+          onAdd={selectedItem => {
+            this.setState({
+              selectedTwo: [...this.state.selectedTwo, selectedItem.value]
+            });
+          }}
+          onRemove={(removedItem, index) => {
+            this.setState({
+              selectedTwo: [
+                ...this.state.selectedTwo.filter(
+                  item => item !== removedItem.value
+                )
+              ]
+            });
+          }}
+          onSelectAll={selectedItems => {
+            this.setState({
+              selectedTwo: [
+                ...this.state.selectedTwo,
+                ...selectedItems.map(item => item.value)
+              ]
+            });
+          }}
+          onRemoveAll={() =>
+            this.setState({
+              selectedTwo: []
+            })
+          }
+        />
+        <br />
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/facuesr/react-multiselect-box/blob/master/src/demo/App.js"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          Source
         </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
